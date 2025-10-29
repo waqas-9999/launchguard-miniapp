a168ece6e46b2051014f9104361504e904a4a629
Muhammad Waqas <waqasshahab1999@gmail.com>
2025-10-29 22:22:53 +0500

    Update StoryProgress.tsx

--- src/components/boost/StoryProgress.tsx
+++ src/components/boost/StoryProgress.tsx
@@ -57,7 +57,7 @@ export default function StoryProgress({ onClose }: StoryProgressProps) {
         style={{
           width: '100%',
           height: '100%',
-          objectFit: 'fill',
+          objectFit: 'cover',
           objectPosition: 'center'
         }}
         initial={{ opacity: 0 }}
